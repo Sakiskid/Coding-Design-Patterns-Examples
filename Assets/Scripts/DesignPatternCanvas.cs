651d63a26f0fbe69bafdea59074f6fbc5100b909
Tyler Drew <33964795+Sakiskid@users.noreply.github.com>
2021-03-19 10:22:57 -0500

    feat: prepares canvas for scripts to initialize data and visualize on screen gui

---
+++ Assets/Scripts/DesignPatternCanvas.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DesignPatternCanvas
+{
+    [SerializeField] private DesignPatternDataSO designPatternData;
+
+    
+}
