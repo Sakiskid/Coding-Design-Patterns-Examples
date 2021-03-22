ed63ec7f466a5fa200870d0e2e33bfa808a41c01
Sakiskid <tylerdrew.home@gmail.com>
2021-03-22 17:28:24 -0500

    feat: sliders update color of prototype ball

--- Assets/Design Patterns/Creational Patterns/Prototype/PrototypeBall.cs
+++ Assets/Design Patterns/Creational Patterns/Prototype/PrototypeBall.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Sprites;
 
-public class PrototypeBall : MonoBehaviour
+public class PrototypeBall : MonoBehaviour, ICloneable
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    SpriteRenderer spriteRenderer;
+
+    private float red, green, blue;
+
+    // Violating DRY here over these update color methods, but for the sake of this example this decreases complexity and prevents any accidental string inputs
+
+    public void UpdateRedColor(Slider slider) {
+        red = slider.value;
+        UpdateColor();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void UpdateBlueColor(Slider slider) {
+        blue = slider.value;
+        UpdateColor();
+    }
+
+    public void UpdateGreenColor(Slider slider) {
+        green = slider.value;
+        UpdateColor();
+    }
+
+    // Get references to components
+    private void Awake() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    /// <summary>Updated whenever Red, Green, or Blue is changed. Sets the color of the sprite. </summary>
+    private void UpdateColor() {
+        Color newColor = new Color(red, green, blue);
+        spriteRenderer.color = newColor;
+    }
+
+    public void Clone() {
+
     }
 }
