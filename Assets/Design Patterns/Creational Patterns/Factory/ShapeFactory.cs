66b84ccafd2af4769b4e9e681eb8e402eaee74a8
Sakiskid <tylerdrew.home@gmail.com>
2021-03-22 10:35:36 -0500

    feat: ShapeFactory contains the creators and interface

---
+++ Assets/Design Patterns/Creational Patterns/Factory/ShapeFactory.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+// Interface for all products of the Factory
+public interface IShape {
+    string Details();
+}
+
+abstract class ShapeCreator
+{
+    // Base method for creator, which will be overrided by sub classes (concrete creators)
+    public abstract IShape FactoryMethod();
+
+    public string CreateShape() {
+        IShape newShape = FactoryMethod();
+        string result = $"ShapeCreator :: Created new shape ({newShape}). Shape details: {newShape.Details()}";
+        return result;
+    }
+}
+
+class SquareCreator : ShapeCreator {
+    public override IShape FactoryMethod() {
+        return new Square();
+    }
+}
+
+class BouncyBallCreator : ShapeCreator {
+    public override IShape FactoryMethod()
+    {
+        return new BouncyBall();
+    }
+}
---
+++ Assets/Design Patterns/Creational Patterns/Factory/Square.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Square : IShape
+{
+    public string Details() {
+        return "created square";
+    }
+}
