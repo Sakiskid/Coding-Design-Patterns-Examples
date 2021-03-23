b07be323082cd4aa152b535f71359a8e02c862d3
Sakiskid <tylerdrew.home@gmail.com>
2021-03-23 12:40:53 -0500

    feat: introduced most of singleton pattern and initialized singleton scene

--- Assets/Design Patterns/Creational Patterns/Singleton/Singleton.cs
+++ Assets/Design Patterns/Creational Patterns/Singleton/Singleton.cs
@@ -1,18 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Singleton : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+namespace DesignPatterns.Singleton {
+    public class Singleton
     {
-        
-    }
+        private static readonly object _lock = new object();
+        private static Singleton instance;
+        // We are using this Value to store an int, for the purposes of testing the singleton
+        public int Value { get; set; }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public static Singleton GetInstance(int value) {
+            if (instance == null) {
+                // This lock only allows ONE thread to access it at a time.
+                lock (_lock)
+                {
+                    if (instance == null) {
+                        instance = new Singleton();
+                        instance.Value = value;
+                    }
+                }
+            }
+            return instance;
+        }
     }
 }
